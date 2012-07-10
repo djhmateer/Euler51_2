cc8ec095376a3cb25119e04b92b1945d0ca4ed14
Dave Mateer <davemateer@gmail.com>
2012-07-10 16:17:40 -0500

    Sorted char conversions

--- Euler51_2/Class1.cs
+++ Euler51_2/Class1.cs
@@ -31,63 +31,75 @@ namespace Euler51_2
         {
             string s = "12345";
             char[] array = s.ToCharArray();
-            array[1] = (char) (3+48);
+            array[1] = (char)(3 + 48);
             return 0;
         }
 
         public static int FindSmallestPrime()
         {
             //assume 6 digits will be required
-            for (int a = 121313; a <= 999999; a = a + 2)
+            for (int a = 121313; a <= 121313; a = a + 2)
             {
                 if (IsPrime(a))
                 {
-                    var listPrimesInFamily = new List<int>();
                     string s = a.ToString();
                     char[] array = s.ToCharArray();
                     //pick a digit to use..but not last digit as this wouldn't allow 8 primes
                     //leading 0's not permitted
                     //looking through for a triplet of numbers in the first 5 digits
                     //that would be exchanged for a total of 8 primes
-                    //-1 means don't change the digit
-                    for (int i = -1; i <= 9; i++)
+                    //0 means don't change the digit
+                    for (int i = 0; i <= 1; i++)
                     {
-                        if (i != -1 && i != 0)
+                        for (int j = 0; j <= 1; j++)
                         {
-                            //change first digit to i
-                            array[0] = (char)(i+48);
-                        }
-                        for (int j = -1; j <= 9; j++)
-                        {
-                            if (j != -1)
+                            for (int k = 0; k <= 1; k++)
                             {
-                                //change second digit to j
-                                array[1] = (char)(j+48);
-                            }
-
-                            for (int k = -1; k <= 9; k++)
-                            {
-                                if (k != -1)
+                                for (int n = 0; n <= 1; n++)
                                 {
-                                    //change third digit to k
-                                    array[2] = (char)(k + 48);
-                                }
-                                string n = new string(array);
-                                int numChanged = Convert.ToInt32(n);
-                                if (IsPrime(numChanged))
-                                {
-                                    if (numChanged != a)
+                                    for (int p = 0; p <= 1; p++)
                                     {
-                                        if (!listPrimesInFamily.Contains(numChanged))
+                                        for (int q = 0; q <= 1; q++)
                                         {
-                                            listPrimesInFamily.Add(numChanged);
+                                            var listPrimesInFamily = new List<int>();
+
+                                            //try changing digits
+                                            for (int m = 0; m <= 9; m++)
+                                            {
+                                                if (i == 1) array[0] = (char)(m + 48);
+                                                if (j == 1) array[1] = (char)(m + 48);
+                                                if (k == 1) array[2] = (char)(m + 48);
+                                                if (n == 1) array[3] = (char)(m + 48);
+                                                if (p == 1) array[4] = (char)(m + 48);
+                                                if (q == 1) array[5] = (char)(m + 48);
+
+                                                string num = new string(array);
+                                                int numChanged = Convert.ToInt32(num);
+                                                if (IsPrime(numChanged))
+                                                {
+                                                    if (numChanged != a)
+                                                    {
+                                                        if (numChanged > 100000)
+                                                        {
+                                                            if (!listPrimesInFamily.Contains(numChanged))
+                                                            {
+                                                                listPrimesInFamily.Add(numChanged);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            Console.WriteLine(listPrimesInFamily.Count);
+                                            if (listPrimesInFamily.Count == 8)
+                                            {
+                                                Console.WriteLine("found");
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                    Console.WriteLine(listPrimesInFamily.Count);
                 }
             }
             return -1;
