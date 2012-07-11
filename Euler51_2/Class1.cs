e099ce6fb7dceb61e73bf1668a245cbf5ef6b5a6
Dave Mateer <davemateer@gmail.com>
2012-07-11 10:04:47 -0500

    Fully working version

--- Euler51_2/Class1.cs
+++ Euler51_2/Class1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Euler51_2
@@ -10,35 +10,21 @@ namespace Euler51_2
     {
         //Find the smallest prime which, by changing the same part of that number
         //can form eight different primes
+        //x2x3x3: 929393 828383 626363 525353 424343 323333 222323 121313
         [Test]
         public void FindSmallestPrime_Given_Return()
         {
             int result = E51.FindSmallestPrime();
             Assert.AreEqual(121313, result);
         }
-
-        [Test]
-        public void MethodUnderTest_scenario_expectedbehaviour()
-        {
-            var result = E51.DoSomething();
-
-        }
     }
 
     public class E51
     {
-        public static int DoSomething()
-        {
-            string s = "12345";
-            char[] array = s.ToCharArray();
-            array[1] = (char)(3 + 48);
-            return 0;
-        }
-
         public static int FindSmallestPrime()
         {
             //assume 6 digits will be required
-            for (int a = 121313; a <= 121313; a = a + 2)
+            for (int a = 100003; a <= 999999; a = a + 2)
             {
                 if (IsPrime(a))
                 {
@@ -66,6 +52,10 @@ namespace Euler51_2
                                             //try changing digits
                                             for (int m = 0; m <= 9; m++)
                                             {
+                                                //reset the array
+                                                s = a.ToString();
+                                                array = s.ToCharArray();
+
                                                 if (i == 1) array[0] = (char)(m + 48);
                                                 if (j == 1) array[1] = (char)(m + 48);
                                                 if (k == 1) array[2] = (char)(m + 48);
@@ -73,26 +63,33 @@ namespace Euler51_2
                                                 if (p == 1) array[4] = (char)(m + 48);
                                                 if (q == 1) array[5] = (char)(m + 48);
 
+                                                //Console.WriteLine("{0},{1},{2},{3},{4},{5}",i,j,k,n,p,q);
+                                                if (i == 1 && k == 1 && p == 1)
+                                                {
+                                                    var tt = 0;
+                                                }
                                                 string num = new string(array);
                                                 int numChanged = Convert.ToInt32(num);
+                                                //Console.WriteLine("c:{0}",num);
                                                 if (IsPrime(numChanged))
                                                 {
-                                                    if (numChanged != a)
+                                                    if (numChanged > 100000)
                                                     {
-                                                        if (numChanged > 100000)
+                                                        if (!listPrimesInFamily.Contains(numChanged))
                                                         {
-                                                            if (!listPrimesInFamily.Contains(numChanged))
-                                                            {
-                                                                listPrimesInFamily.Add(numChanged);
-                                                            }
+                                                            listPrimesInFamily.Add(numChanged);
                                                         }
                                                     }
                                                 }
                                             }
-                                            Console.WriteLine(listPrimesInFamily.Count);
                                             if (listPrimesInFamily.Count == 8)
                                             {
-                                                Console.WriteLine("found");
+                                                Console.WriteLine("{1}, {0}", listPrimesInFamily.Count, a);
+                                                foreach (var i1 in listPrimesInFamily)
+                                                {
+                                                    Console.WriteLine("  {0}", i1);
+                                                }
+                                                return listPrimesInFamily.OrderBy(x => x).FirstOrDefault();
                                             }
                                         }
                                     }
